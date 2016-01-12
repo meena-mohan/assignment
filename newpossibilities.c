d77c730aa975c6a6541470961306855fcdc0c821
meena-mohan <m.meenamohan1995@gmail.com>
2016-01-12 18:57:55 +0530

    Update newpossibilities.c

--- newpossibilities.c
+++ newpossibilities.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
-int main(void)
+int main(void) 
 {
-	int a[10],i,n,j;
-	scanf("%d\n",&n);
-	for(i=0;i<n;i++)
+	int a[10],i,j,n,k,l,m,n1;
+	scanf("%d",&n);
+	for(k=0;k<n;k++)
 	{
-		scanf("%d",&a[i]);
-		printf("%d ",a[i]);
+		scanf("%d",&a[k]);
+		printf("%d\n",a[k]);
 	}
-	printf("\n");
-	for(i=0;i<n;i++)
-	{
-		printf("%d\n",a[i]);
-		for(j=i+1;j<n;j++)
-		{
-			if((j>i)||(j<i))
-			{
-				printf("%d %d\n",a[i],a[j]);
-			}
-		}
+	for(i=2;i<=n;i++)
+	{ 
+	  for(j=0;j<=n-i;j++)
+	  {
+	       l=j+i-1;
+	       for(m=l;m<n;m++)
+	       {
+	  	     for(n1=j;n1<l;n1++)
+	  	     {
+	  	     	 printf("%d ",a[n1]);
+	  	     }
+	             printf("%d\n",a[m]);
+	       }
+	  }
 	}
 	return 0;
 }
+
